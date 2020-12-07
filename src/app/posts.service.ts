9aa488bf1659bf2296a2514d33cff1cdc2832cc4
Jaymeweiss <jaymeweiss@gmail.com>
2020-12-07 16:06:12 +0200

    add dummy catch error note

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core';
 import {Post} from './post.model';
 import {HttpClient} from '@angular/common/http';
-import {map} from 'rxjs/operators';
-import {Observable, Subject} from 'rxjs';
+import {catchError, map} from 'rxjs/operators';
+import {Observable, Subject, throwError} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -28,14 +28,20 @@ export class PostsService {
   fetchPosts(): Observable<any> {
     return this.httpClient.get('https://ng-complete-guide-c9a62-default-rtdb.firebaseio.com/posts.json')
       .pipe(map((responseData: { [key: string]: Post }) => {
-        const postArray: Post[] = [];
-        for (const key in responseData) {
-          if (responseData.hasOwnProperty(key)) {
-            postArray.push({...responseData[key], id: key});
+          const postArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postArray.push({...responseData[key], id: key});
+            }
           }
-        }
-        return postArray;
-      }));
+          return postArray;
+        }),
+        catchError(errorResponse => {
+          // Send to logging / analytics server
+          // Pass it on
+          return throwError(errorResponse);
+        })
+      );
   }
 
   deleteAllPosts(): Observable<any> {
