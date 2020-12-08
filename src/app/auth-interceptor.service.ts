2fe32564575d6bb2821573593c0ee55de027db91
Jaymeweiss <jaymeweiss@gmail.com>
2020-12-08 10:14:51 +0200

    modifying the response

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +17,11 @@ export class AuthInterceptorService implements HttpInterceptor {
       // do something before the request is sent
       modifiedRequest = req.clone({headers: req.headers.append('Auth', 'xyz')});
     }
-    return next.handle(modifiedRequest);
+    return next.handle(modifiedRequest).pipe(tap( event => {
+      if (event.type === HttpEventType.Response) {
+        console.log('Response arrived, body data: ');
+        console.log(event.body);
+      }
+    }));
   }
 }
